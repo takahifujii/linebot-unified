5bc1660a1efbe76beb79e7a4619e6eafb908952b
takahifujii <220110032+takahifujii@users.noreply.github.com>
2026-03-12 15:20:45 +0900

    Fix console log syntax for server port message

--- index.js
+++ index.js
@@ -676,5 +676,5 @@ app.get('/logtest', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   
-  console.log(\`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
