afe43c1c2a0f8348fc64cae1e6e51635a5d9383d
takahifujii <220110032+takahifujii@users.noreply.github.com>
2026-03-16 21:35:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1140,5 +1140,5 @@ app.get('/inventory', (req, res) => {
 // ------------------------------------------
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(\`Server is running on port \${PORT}\`);
+  console.log(\`Server is running on port ＋　PORT`);
 });
